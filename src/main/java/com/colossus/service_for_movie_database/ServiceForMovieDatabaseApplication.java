a0793671a08704737bdc6d02e9447fafc31d4b7b
Kirill Kosykh <triplesonicslash@list.ru>
2023-06-18 20:49:50 +0300

    init commit

---
+++ src/main/java/com/colossus/service_for_movie_database/ServiceForMovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.colossus.service_for_movie_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForMovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForMovieDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colossus/service_for_movie_database/ServiceForMovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colossus.service_for_movie_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceForMovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
